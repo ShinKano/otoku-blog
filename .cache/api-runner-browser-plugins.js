8b54498b07856c1943e6be621916d52e154dad77
kanoshinnosuke <yushph@gmail.com>
2019-10-20 23:32:52 +0800

    added google analytics plugin

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/Shinnosuke/Documents/b5react/otoku/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-83299337-3","head":true,"exclude":["/preview/**","/do-not-track/me/too/"],"pageTransitionDelay":0},
+    },{
       plugin: require('/Users/Shinnosuke/Documents/b5react/otoku/node_modules/gatsby-remark-images/gatsby-browser.js'),
       options: {"plugins":[],"maxWidth":2048},
     },{
--- gatsby-config.js
+++ gatsby-config.js
@@ -13,18 +13,10 @@ module.exports = {
           trackingId: "UA-83299337-3",
           // Defines where to place the tracking script - `true` in the head and `false` in the body
           head: true,
-          // Setting this parameter is optional
-          anonymize: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Delays sending pageview hits on route update (in milliseconds)
           pageTransitionDelay: 0,
-          // Any additional optional fields
-          sampleRate: 5,
-          siteSpeedSampleRate: 10,
-          cookieDomain: "example.com",
         },
     },
     {
