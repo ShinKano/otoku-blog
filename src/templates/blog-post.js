ea2c9aa3736e1d5b587029e9d4f456608b706d21
kanoshinnosuke <yushph@gmail.com>
2019-10-22 15:52:49 +0800

    add css to blog-content

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,7 +26,11 @@ export const BlogPostTemplate = ({
               {title}
             </h1>
             <p>{description}</p>
-            <PostContent content={content} />
+
+            <div className="blog-content">
+                <PostContent content={content} />
+            </div>
+            
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
                 <h4>Tags</h4>
